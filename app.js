9159adeaa0d1abd55c08af80a87b5acb146c594f
aw1.alexwood@gmail.com <alex.wood@picmonic.com>
2015-12-21 00:24:02 -0700

    functional search

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var http = require("http");
 var https = require("https");
+var Twitter = require('twitter');
 var app = express();
+
+
+var twitterClient = new Twitter({
+  consumer_key: 'xxxxxx',
+  consumer_secret: 'xxxxxx',
+  access_token_key: 'xxxxxx',
+  access_token_secret: 'xxxxxx'
+});
 
 app.use(express.static('public')); //todo nginx in production
 app.use(function(request, response, next) {
@@ -14,25 +23,27 @@ app.get('/', function(request, response){
 }); 
 
 app.get('/api/v1/twitter/post/statuses/update/:status', function(request, response){
-
-	var Twitter = require('twitter');
  
-var client = new Twitter({
-  consumer_key: 'XXX',
-  consumer_secret: 'XXX',
-  access_token_key: 'XXX',
-  access_token_secret: 'XXX'
-});
- 
-client.post('statuses/update', {status: request.params.status},  function(error, tweet, response){
-  if(error) throw error;
-  console.log(tweet);  // Tweet body. 
-  console.log(response);  // Raw response object. 
-});
+	twitterClient.post('statuses/update', {status: request.params.status},  function(error, tweet, response){
+	  if(error) throw error;
+	  console.log(tweet);  // Tweet body. 
+	  console.log(response);  // Raw response object. 
+	});
 
 	//response.send(resp.statusCode);
 	response.end();
 });
+
+app.get('/api/v1/twitter/search/:search', function(request, response){
+	twitterClient.get('search/tweets', {q: request.params.search}, function(error, tweets, response){
+   		console.log(tweets);
+	});
+
+	//response.send(resp.statusCode);
+	response.end();
+});
+
+
 
 app.listen(1337, function(){
 	console.log('listening on port 1337');	
