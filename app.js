479563c6a3bf25804b4484e52b8d8967f07e4f0c
aw1.alexwood@gmail.com <alex.wood@picmonic.com>
2015-12-20 23:32:23 -0700

    makes api endpoint dynamic

--- app.js
+++ app.js
@@ -13,18 +13,18 @@ app.get('/', function(request, response){
 	response.sendFile(__dirname + '/public/index.html');
 }); 
 
-app.get('/api/v1/twitter/count-mentions/:mentions', function(request, response){
+app.get('/api/v1/twitter/post/statuses/update/:status', function(request, response){
 
 	var Twitter = require('twitter');
  
 var client = new Twitter({
-  consumer_key: 'SCGiI36qKuzOubIT8LtSNDbJq',
-  consumer_secret: 'XGUCOopoIsgr432qqWIPihABlf9vHmmm77iwcoUxHbkORFIKgZ',
-  access_token_key: '3165869263-MWZFHU2K7KPyh3armcu22q3OWdPz9BUdNQnK5zs',
-  access_token_secret: 'yNl7T6mBhKhmlha34KKZ4zDyEBsxtJI1RqYlcfHP8OCKi'
+  consumer_key: 'XXX',
+  consumer_secret: 'XXX',
+  access_token_key: 'XXX',
+  access_token_secret: 'XXX'
 });
  
-client.post('statuses/update', {status: 'Hello World - testing bundleslang! #nodejs'},  function(error, tweet, response){
+client.post('statuses/update', {status: request.params.status},  function(error, tweet, response){
   if(error) throw error;
   console.log(tweet);  // Tweet body. 
   console.log(response);  // Raw response object. 
