c3d17ae768746b4836faa273867da2fd6de7d093
aw1.alexwood@gmail.com <alex.wood@picmonic.com>
2015-12-30 23:28:40 -0700

    adds restful api endpoint to return chart data by ticker symbol

--- app.js
+++ app.js
@@ -97,6 +97,20 @@ app.get('/api/v1/markit/search/quote/:ticker', function(request, response){
     response.end();
 });
 
+app.get('/api/v1/markit/search/interactive/:ticker', function(request, response){
+
+    fetch('http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=%7B%22Normalized%22%3Afalse%2C%22NumberOfDays%22%3A365%2C%22DataPeriod%22%3A%22Day%22%2C%22Elements%22%3A%5B%7B%22Symbol%22%3A%22' + request.params.ticker + '%22%2C%22Type%22%3A%22price%22%2C%22Params%22%3A%5B%22c%22%5D%7D%5D%7D')
+        .then(function(res) {
+            console.log(res);
+            return res.json();
+        }).then(function(json) {
+            console.log(json);
+        });
+
+    //response.send(resp.statusCode);
+    response.end();
+});
+
 app.get('/api/v1/google-news/search/:search', function(request, response){
     googleNews = new GoogleNews();
 
