a899892fa99e160f216a20a8564b0f681e8c6b95
Alex <aw1.alexwood@gmail.com>
2016-03-07 11:20:25 -0700

    refactors app to streaming rather than callbacks

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var http = require("http");
 var https = require("https");
+var request = require('request');
 var Twitter = require('twitter');
-var GoogleNews, googleNews, track;
-var GoogleNews = require('google-news')
-var fetch = require('node-fetch');
 var env = require('./env.js');
 var fs = require('fs');
 
@@ -12,13 +10,14 @@ var app = express();
 
 
 var twitterClient = new Twitter({
-    consumer_key: "xfkjzRkTIKc33Z0l6KWySl56U",
-    consumer_secret: "JuOetPqYVC929L9O42erKfX0aF6Dqb9ph1pJ9P684duiVn4CB8",
-    access_token_key: "3165869263-MWZFHU2K7KPyh3armcu22q3OWdPz9BUdNQnK5zs",
-    access_token_secret: "yNl7T6mBhKhmlha34KKZ4zDyEBsxtJI1RqYlcfHP8OCKi"
+    consumer_key: env.TWITTER_CONSUMER_KEY,
+    consumer_secret: env.TWITTER_CONSUMER_SECRET,
+    access_token_key: env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 app.use(express.static('public')); //todo nginx in production
+
 app.use(function(req, res, next) {
     next();
 });
@@ -58,78 +57,9 @@ app.get('/api/v1/twitter/mentions', function(req, res){
     res.end();
 });
 
-app.get('/api/v1/reddit/search/:search', function(req, res){
-
-    fetch('https://www.reddit.com/search.json?q='+ req.params.search +'&sort=new')
-        .then(function(res) {
-            return res.json();
-        }).then(function(json) {
-            console.log(json);
-        });
-
-    //res.send(resp.statusCode);
-    res.end();
-});
-
-app.get('/api/v1/markit/search/:search', function(req, res){
-
-    fetch('http://dev.markitondemand.com/MODApis/Api/v2/Lookup/jsonp?input=' + req.params.search)
-        .then(function(res) {
-            console.log(res);
-            return res.json();
-        }).then(function(json) {
-            console.log(json);
-        });
-
-    //res.send(resp.statusCode);
-    res.end();
-});
-
-app.get('/api/v1/markit/search/quote/:ticker', function(req, res){
-
-    fetch('http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=' + req.params.ticker)
-        .then(function(res) {
-            console.log(res);
-            return res.json();
-        }).then(function(json) {
-            console.log(json);
-        });
-
-    //res.send(resp.statusCode);
-    res.end();
-});
-
-app.get('/api/v1/markit/search/interactive/:ticker', function(req, res){
-
-    fetch('http://dev.markitondemand.com/MODApis/Api/v2/InteractiveChart/json?parameters=%7B%22Normalized%22%3Afalse%2C%22NumberOfDays%22%3A365%2C%22DataPeriod%22%3A%22Day%22%2C%22Elements%22%3A%5B%7B%22Symbol%22%3A%22' + req.params.ticker + '%22%2C%22Type%22%3A%22price%22%2C%22Params%22%3A%5B%22c%22%5D%7D%5D%7D')
-        .then(function(res) {
-            console.log(res);
-            return res.json();
-        }).then(function(json) {
-            console.log(json);
-        });
-
-    //res.send(resp.statusCode);
-    res.end();
-});
-
 app.get('/api/v1/google-news/search/:search', function(req, res){
-    googleNews = new GoogleNews();
-
-
-    googleNews.stream(req.params.search, function(stream) {
-
-        stream.on(GoogleNews.DATA, function(data) {
-            return console.log('Data Event received... ' + data.title);
-        });
-
-        stream.on(GoogleNews.ERROR, function(error) {
-            return console.log('Error Event received... ' + error);
-        });
-    });
-
-    //res.send(resp.statusCode);
-    res.end();
+    var url='http://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=' + req.params.search;
+    request(url).pipe(res);
 });
 
 app.listen(7777, function(){
