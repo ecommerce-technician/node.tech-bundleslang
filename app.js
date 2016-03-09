e3855cfe2d75c4ad01ec7b944118345206e778f4
Alex <aw1.alexwood@gmail.com>
2016-03-09 10:57:37 -0700

    added reddit endpoint

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
 
+//Twitter
 app.get('/api/v1/twitter/post/statuses/update/:status', function(req, res){
     twitterClient.post('statuses/update', {status: req.params.status},  function(error, tweet, res){
         if(error) throw error;
@@ -38,6 +39,7 @@ app.get('/api/v1/twitter/post/statuses/update/:status', function(req, res){
     res.end();
 });
 
+//Twitter
 app.get('/api/v1/twitter/search/:search', function(req, res){
     twitterClient.get('search/tweets', {q:req.params.search,result_type:'recent',count:99,lang:'en'}, function(error, tweets, res){
         shipIt(tweets)
@@ -48,6 +50,7 @@ app.get('/api/v1/twitter/search/:search', function(req, res){
     }
 });
 
+//Twitter
 app.get('/api/v1/twitter/mentions', function(req, res){
     twitterClient.get('statuses/mentions_timeline', {result_type:'recent'}, function(error, tweets, res){
         console.log(tweets.statuses);
@@ -57,10 +60,18 @@ app.get('/api/v1/twitter/mentions', function(req, res){
     res.end();
 });
 
+//Google News
 app.get('/api/v1/google-news/search/:search', function(req, res){
     var url='http://ajax.googleapis.com/ajax/services/search/news?v=1.0&q=' + req.params.search;
     request(url).pipe(res);
 });
+
+//Reddit
+app.get('/api/v1/google-news/search/:search', function(req, res){
+    var url='http://www.reddit.com/search.json?q=' + req.params.search + 'ferrari&sort=new';
+    request(url).pipe(res);
+});
+
 
 app.listen(7777, function(){
     console.log('listening on port 7777');
